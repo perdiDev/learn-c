ed8c33a4b4922925017c173c13e6fab22dfb1d56
PerdiDev <perdidev@gmail.com>
2023-03-20 16:27:34 +0800

    Update tugas matdis

--- matdis/invers_matriks_2x2.c
+++ matdis/invers_matriks_2x2.c
@@ -1,39 +1,87 @@
 #include <stdio.h>
 
+void tulis_matriks(int m[2][2]);
+void cetak_matriks(int m[2][2]);
+void cetak_adj(float adj[2][2]);
+void mencari_invers(float invers[2][2],float adj[2][2], int det);
+void cetak_invers(float in[2][2]);
+
 int main(int argc, char const *argv[]){
-    int a, b, c, d, e, det;
-    float adja, adjb, adjc, adjd;
-    float invers, ina, inb, inc, ind;
+    //Variabel
+    int matriks[2][2],a, det;
+    float adj[2][2],invers[2][2];
 
     //Matriks
-    printf("Masukkan angka a : ");
-    scanf("%d", &a);
-    printf("Masukkan angka b : ");
-    scanf("%d", &b);
-    printf("Masukkan angka c : ");
-    scanf("%d", &c);
-    printf("Masukkan angka d : ");
-    scanf("%d", &d);
-    printf("|%d\t%d|\n|%d\t%d|\n",a,b,c,d);
+    tulis_matriks(matriks);
+    
+    //Output nilai matriks
+    printf("\nMatriks :\n");
+    cetak_matriks(matriks);
 
-    //determinan
-    det=(a*d)-(b*c);
-    printf("Determinan = %d", det);
+    //Determinan
+    det=(matriks[0][0]*matriks[1][1])-(matriks[0][1]*matriks[1][0]);
+    printf("Determinan = %d\n", det);
 
     //adj
-    e=a;
-    adja=d;
-    adjb=-1*b;
-    adjc=-1*c;
-    adjd=e;
-    printf("\n|%.0f\t%.0f|\n|%.0f\t%.0f|\n", adja,adjb, adjc,adjd);
+    a=matriks[0][0];
+    adj[0][0]=matriks[1][1];
+    adj[0][1]=-1*matriks[0][1];
+    adj[1][0]=-1*matriks[1][0];
+    adj[1][1]=a;
+    printf("adj : \n");
+    cetak_adj(adj);
 
     //Invers
-    ina=1*adja/det;
-    inb=1*adjb/det;
-    inc=1*adjc/det;
-    ind=1*adjd/det;
-    printf("Invers : \n");
-    printf("|%.2f\t%.2f|\n|%.2f\t%.2f|", ina, inb, inc, ind);
+    if (det!=0){
+        mencari_invers(invers,adj,det);
+        printf("\nInvers :\n");
+        cetak_invers(invers);
+    }else{
+        printf("Invers tidak ada");
+    }
     return 0;
+}
+void tulis_matriks(int m[][2]) {
+    char n='a';
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Masukkan Nilai %c: ", n);
+            scanf("%d", &m[i][j]);
+            n++;
+        }
+    }
+}
+void cetak_matriks(int m[2][2]){
+    for (int i = 0; i < 2; i++) {
+        printf("| ");
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", m[i][j]);
+        }
+        printf("|\n");
+    }
+}
+void cetak_adj(float adj[2][2]){
+    for (int i = 0; i < 2; i++) {
+        printf("| ");
+        for (int j = 0; j < 2; j++) {
+            printf("%.0f ", adj[i][j]);
+        }
+        printf("|\n");
+    }
+}
+void mencari_invers(float invers[2][2], float adj[2][2], int det){
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<2; j++) {
+            invers[i][j] = 1*adj[i][j]/det;
+        }
+    }
+}
+void cetak_invers(float in[2][2]){
+    for (int i = 0; i < 2; i++) {
+        printf("| ");
+        for (int j = 0; j < 2; j++) {
+            printf("%.2f ", in[i][j]);
+        }
+        printf("|\n");
+    }
 }
